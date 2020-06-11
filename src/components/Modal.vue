5a70677c918ceeddac0134246f6ac6bf8a61dddb
Tim <timdev89@gmail.com>
2020-06-11 11:38:33 -0400

    pushing to github

---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="modal">
+    <div class="modal__background"></div>
+    <div class="modal__card-wrapper">
+      <div class="modal__card">
+        <h4 class="modal__card-heading">
+          {{ employee.name }}
+        </h4>
+        <div class="modal__card-details">
+          <ul>
+            <li>Username: {{ employee.username }}</li>
+            <li>Email: {{ employee.email }}</li>
+            <li>Address:
+               {{ employee.address.street }},
+               {{ employee.address.suite }},
+               {{ employee.address.city }},
+               {{ employee.address.zipcode }}
+            </li>
+            <li>Phone: {{ employee.phone }}</li>
+            <li>Website: {{ employee.website }}</li>
+            <li>Company: {{ employee.company.name }} <br>
+                "{{ employee.company.catchPhrase }}"
+            </li>
+          </ul>
+          <a @click="modalClose">Exit</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['employee'],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    modalClose() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <header class="header">
+
+      <div class="header__text-box">
+        <h1 class="heading-primary">
+          <span class="heading-primary--main">EmploYEET</span>
+          <span class="heading-primary--sub">Yeet someone up, today!</span>
+        </h1>
+      </div>
+
+      <a href="#" class="btn btn--white">Find Employees</a>
+
+    </header>
+
+    <section class="employee__box">
+
+      <div class="employee__card-wrapper">
+        <div v-for="employee in data" :key="employee.id" class="employee__card">
+          <h4 class="employee__card-heading">
+            {{ employee.name }}
+          </h4>
+          <div class="employee__card-details">
+            <ul>
+              <li>Username: {{ employee.username }}</li>
+              <li>Email: {{ employee.email }}</li>
+              <li>Phone: {{ employee.phone }}</li>
+              <li>Website: {{ employee.website }}</li>
+            </ul>
+            <a @click="selectedEmployee = employee">view more</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Modal v-if="employeeSelected" :employee="selectedEmployee" @close="closeModal()"></Modal>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Modal from '@/components/Modal'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      data: {},
+      selectedEmployee: {}
+    }
+  },
+  computed: {
+    employeeSelected() {
+      if (Object.keys(this.selectedEmployee).length > 0) {
+        document.body.style.overflow = 'hidden'
+        return true
+      } else {
+        document.body.style.overflow = 'auto'
+        return false
+      }
+    }
+  },
+  created() {
+    this.getEmployees()
+  },
+  methods: {
+    async getEmployees() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      this.data = await res.json()
+    },
+    closeModal() {
+      this.selectedEmployee = {}
+    }
   }
 }
 </script>
+
+<style lang="scss">
+@import "src/scss/main.scss";
+
+</style>
